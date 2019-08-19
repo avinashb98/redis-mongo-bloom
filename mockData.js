6154c4220185e416ab53e0e227f948b290a5e889
Avinash Bhardwaj <avinashb97@gmail.com>
2019-08-19 20:39:32 +0530

    update mocking script to include bloom filter

--- mockData.js
+++ mockData.js
@@ -1,21 +1,30 @@
 const readline = require('readline');
+const fs = require('fs');
 const mongo = require('./config/db');
 const User = require('./user');
+const BloomFilter = require('./userbloom');
 
-const UPPER_LIMIT = 8999999999999999;
-const LOWER_LIMIT = 2999999999999999;
+const MAX_USERS = 2000000;
+const UPPER_LIMIT = 6999999999999999;
+const LOWER_LIMIT = 6000000000000000;
 const getRandomUserId = () => Math.floor(
     (Math.random() * (UPPER_LIMIT - LOWER_LIMIT)) + LOWER_LIMIT
 );
 
 // (async () => {
 //     await mongo.connect();
-//     for (let i = 0; i < 200000; i += 1) {
+//     await BloomFilter.connect();
+//     console.log('Filter Connected: userbloom');
+
+//     const ids = [];
+//     for (let i = 0; i < MAX_USERS; i += 1) {
 //         readline.clearLine(process.stdout);
 //         readline.cursorTo(process.stdout, 0);
-//         process.stdout.write(`Done: ${i + 1} / 200000`);
+//         process.stdout.write(`Done: ${i + 1} / ${MAX_USERS}`);
 //         const userId = getRandomUserId();
-//         await User.create({ userId });
+//         ids.push(userId);
+//         await Promise.all([User.create({ userId }), BloomFilter.add(userId)]);
 //     }
 //     console.log('\n');
+//     fs.writeFileSync('userIds.json', JSON.stringify(ids));
 // })();
